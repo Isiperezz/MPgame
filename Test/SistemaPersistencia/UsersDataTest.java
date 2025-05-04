f2c408da1d86b01f30532b536c8b04becbe89b17
Pablo Ruiz Uroz <lttpablo@gmail.com>
2025-05-04 21:20:23 +0200

    Agregar JUnit 5.8.1 y configurar el directorio de pruebas en el archivo de configuración del módulo

---
+++ Test/SistemaPersistencia/UsersDataTest.java
@@ -0,0 +1,24 @@
+package SistemaPersistencia;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UsersDataTest {
+
+    @Test
+    void getPersonajesJugadores() {
+    }
+
+    @Test
+    void setPersonajesJugadores() {
+    }
+
+    @Test
+    void getUsuarios() {
+    }
+
+    @Test
+    void getLogin() {
+    }
+}
